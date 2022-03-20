80833e7756827c46c7f1982a5b2006e0bd8098dd
Matt Fellenz <matt@felle.nz>
2022-03-20 14:04:21 -0700

    Fix x.sh CWD error

--- x.sh
+++ x.sh
@@ -6,13 +6,13 @@ case $1 in
 		./x.sh dev-server &
 	;;
 	dev-client)
-		(cd client && npx parcel watch --dist-dir ./client/build ./client/src/index.html)
+		npx parcel watch --dist-dir ./client/build ./client/src/index.html
 	;;
 	dev-server)
 		(cd server && python backend.py)
 	;;
 	build-client)
-		(cd client && npx parcel build --dist-dir ./client/build ./client/src/index.html)
+		npx parcel build --dist-dir ./client/build ./client/src/index.html
 	;;
 	*)
 		echo "Actions must be one of: dev dev-client dev-server build-client" >&2
