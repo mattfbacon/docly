6e30cb95f4c3faf3bd9ce69046d9512a3816a5ed
seadeef <kevin@drymail.com>
2022-03-20 17:37:30 -0700

    Kevin changes

--- server/backend.py
+++ server/backend.py
@@ -1,16 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, send_file
 from ocr import OCR
 from gdrive import GDrive
 
 ocr = OCR()
 
 app = Flask(__name__,
-            static_url_path="", 
-            static_folder="client/build/")
+            static_url_path="")
+
+@app.route("/")
+def root():
+    return send_file("static/index.html")
+
 
 @app.route("/api/submit", methods=["POST"])
 def submit():
     json = request.json
-    text = ocr.get_text(json["image"])
-    gdrive = GDrive(json["auth"])
-    gdrive.make_doc(text)
+    info = ocr.get_info(json["image"])
+    print(info)
+    # gdrive = GDrive(json["auth"], text)
+    # gdrive.make_doc(text)
+
+app.run("localhost", 3000)
