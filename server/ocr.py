e8c973b6096d9388787afa062c0813029d4c7423
seadeef <kevin@drymail.com>
2022-03-20 17:03:26 -0700

    Finish OCR

--- server/ocr.py
+++ server/ocr.py
@@ -1,5 +1,7 @@
 from google.cloud import vision
+import os, io
 import base64
+import string
 
 class OCR:
 	def __init__(self):
@@ -7,61 +9,59 @@ class OCR:
 
 	def get_info(self, bytes):
 		img = self.get_img(bytes)
-		return (self.get_text(img), self.get_title(img))
-		
+		response = self.client.document_text_detection(image=img)
 
-	def get_text(self, img):
-		response = client.document_text_detection(image=image)
-			words = self.get_words(response)
-
-			for i in range(len(words)):
-				try:
-					if words[i+1].isalpha() or len(words[i+1]) > 1:
-						words[i] += " "
-				except IndexError:
-					continue
-
-			return "".join(words)
-			
-	def get_title(self, img):
-		response = client.text_detection(image=image)
-		texts = response.text_annotations
-
-		for text in texts:
-			vertices = text.bounding_poly.vertices
-
-
-		
-	def get_words(self, response):
 		words = []
+		bounds = []
+		
 		for page in response.full_text_annotation.pages:
 			for block in page.blocks:
 				for paragraph in block.paragraphs:
-					print("AAA")
 					for word in paragraph.words:
-						if word.confidence > 0.9:
+						if word.confidence > 0.8:
 							word_text = ''.join([
 			                        symbol.text for symbol in word.symbols
 			                    ])
 							words.append(word_text)
-		return words
+							bounds.append(word.bounding_box)
 
+		for i in range(len(words)):
+			try:
+				if words[i+1].isalpha() or len(words[i+1]) > 1:
+					words[i] += " "
+			except IndexError:
+				continue
 
+		title=None
+		found = False
+		for i in range(1, len(bounds)):
+			cur_vertices = bounds[i].vertices
+			prev_vertices = bounds[i-1].vertices
+
+			cur_height = abs(cur_vertices[0].y-cur_vertices[2].y)
+			prev_height = abs(prev_vertices[0].y-prev_vertices[2].y)
+
+			cur_y = cur_vertices[2].y
+			prev_y = prev_vertices[2].y
+
+			#print(words[i-1], prev_height-cur_height, cur_y-prev_y)
+			if cur_y-prev_y > 20:
+				if not found:
+					if prev_height-cur_height > 15:
+						found = True
+						title = "".join(words[:i]).strip()
+
+				words[i] = "\n" + words[i]
+			
+		text = "".join(words).strip()
+		return (text, title)
 
 	def get_img(self, bytes):
-		content = base64.b64decode(bytes)
-		return vision.Image(content=content)
+			content = base64.b64decode(bytes)
+			return vision.Image(content=content)
+			# file_name = os.path.abspath('test/test13.jpg')
 
+			# with io.open(file_name, 'rb') as image_file:
+			# 	content = image_file.read()
 
-"""
-response = client.text_detection(image=image)
-texts = response.text_annotations
-
-for text in texts:
-        print('\n"{}"'.format(text.description))
-
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
-"""
+			# return vision.Image(content=content)
